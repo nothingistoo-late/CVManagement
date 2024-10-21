eacb174bcce4150417a92f6679ec7f1a94c59ff3
ntphu <hctrung2k4@gmail.com>
2024-10-21 17:23:31 +0700

    Update 21/10/2024

---
+++ MyCV/Entities/CV.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCV.Entities
+{
+    public class CV
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Yob { get; set; }
+        public string Position { get; set; } // FE, BE...
+
+
+        // ta không làm toString() lý do là:
+        // cái lưới nó tự trích ra từng colum qua cái hàm getProperty, ta đâu cần ghép chuỗi xong rồi lại mất công cắt
+        // toString() hay dành cho debug, ghi log file, để kiểm tra data đúng sai lúc runtime!!!
+        public CV() { }
+    }
+}
--- MyCV/MainWindow.xaml.cs
+++ MyCV/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using MyCV.Entities;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,23 @@ namespace MyCV
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            // ta sẽ lấy cbi nhiều data đổ vào cái lưới 
+            // data: đến từ data base 
+            // cất trữ vào trong list<CV> List<Student>
+            // cái lưới tên nó là CVListDataGrid rất là chuyên nghiệp, đằng sau nó chính là class CVListDataGrid chỉ cần đưa Data qua property. ItemsSource = list mình chuẩn bị, thế là nó tự generate colum, tự show data vào đúng hàng cột. Nếu mình có 1 List Student(id, name, yob, gpa), listCV(id, name, yob, position..) thì cái grid này tự biết tách cột tách dòng
+            // mỗi sv, cv là 1 dòng của grid
+            // cột trong sv, trong sv là cột của grid
+
+            List<CV> arr = new();
+            // hoặc là dùng mảng nhưng kh chơi mảng nữa
+            arr.Add(new CV() { Id = "CFE01", Name = "An Nguyen", Yob = 2004, Position = "Front-end Intern" });
+            arr.Add(new CV() { Id = "CBE01", Name = "Binh Le", Yob = 2004, Position = "Back-end Intern" });
+            arr.Add(new CV() { Id = "CFTNET01", Name = "Chi Trung", Yob = 2004, Position = "Full-Stack .NET" });
+            CVListDataGrid.ItemsSource = arr;
         }
     }
 }
